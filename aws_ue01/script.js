16d92e029c0f0044e7d7876cfc55b727d220fcad
Carolin Stolpe <cstolpe@diw.de>
2017-05-17 17:42:47 +0200

    erste Funktionalität

--- aws_ue01/script.js
+++ aws_ue01/script.js
@@ -1,5 +1,28 @@
 $( document ).ready(function() {
-	
-    // $("#detail_1").click();
+
+    setVoltage();
+    setAmperage();
+    warningMaxAmpereage();
 
 });
+
+function setVoltage(){
+    $('#voltage').text(genRandomNumber(220, 240));
+}
+
+function setAmperage(){
+    max = parseFloat($('#maxAmperage').text()) + 5 ;
+    $('#amperage').text(genRandomNumber(0, max));
+}
+
+function genRandomNumber(min, max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function warningMaxAmpereage(){
+    currentAmperage = parseFloat($('#amperage').text());
+    maxAmperage = parseFloat($('#maxAmperage').text());
+    if(currentAmperage > maxAmperage){
+        $('#warningMaxAmperage').append('<p>Warnung! Aktueller Belastungswert liegt über dem Maximalwert. Kontaktieren Sie einen Techniker.</p>')
+    }
+}
